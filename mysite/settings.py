bbffbd65fb5bf9107588afe0d9b75fd0089be3bf
O La <tasks.is.great@defigut.de>
2024-05-21 13:43:14 +0200

    next update

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-b4x#9!ti)jb)bshyju2stdecx-ak$-)*rc4(^_@u3!lp52hm3a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.pythonanywhere.com']
 
 
 # Application definition
